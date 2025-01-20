81c06e4b9fc19434654d50fb98a48abb6e9e32c9
Maren Mahsereci <maren.mhsrc@gmail.com>
2025-01-20 22:58:26 +0100

    adding evaluate function to kernel embedding

--- kernel_embedding_dictionary/embeddings/embedding.py
+++ kernel_embedding_dictionary/embeddings/embedding.py
@@ -2,14 +2,22 @@
 # SPDX-License-Identifier: MIT
 
 
+from typing import Callable
+
+import numpy as np
+
 from ..kernels import Kernel
 from ..measures import Measure
 
 
 class KernelEmbedding:
-    def __init__(self, kernel: Kernel, measure: Measure):
+    def __init__(self, kernel: Kernel, measure: Measure, eval_func: Callable):
         self._kernel = kernel
         self._measure = measure
+        self._evaluate = eval_func
+
+    def evaluate(self, x: np.ndarray) -> np.ndarray:
+        return self._evaluate(x, self._kernel, self._measure)
 
     def __str__(self) -> str:
 
