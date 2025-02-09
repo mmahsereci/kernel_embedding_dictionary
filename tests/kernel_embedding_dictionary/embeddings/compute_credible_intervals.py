932b7eef204329df77d53719e5e555357dab1202
Maren Mahsereci <maren.mhsrc@gmail.com>
2025-02-09 22:45:14 +0100

    integral tests

---
+++ tests/kernel_embedding_dictionary/embeddings/compute_credible_intervals.py
@@ -0,0 +1,48 @@
+# Copyright 2025 The KED Authors. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+
+
+import numpy as np
+
+from kernel_embedding_dictionary._get_embedding import get_embedding
+
+from test_embeddings import get_config_expquad_lebesgue_1d_1, get_config_expquad_lebesgue_1d_2
+
+embedding_to_config_list_dict = {
+    "expquad-lebesgue": [get_config_expquad_lebesgue_1d_1, get_config_expquad_lebesgue_1d_2]
+}
+
+if __name__ == "__main__":
+    np.random.seed(0)
+
+    n = int(1e5)
+    ff = 3
+
+    # Choose embedding
+    embedding = "expquad-lebesgue"
+    get_config_func_list = embedding_to_config_list_dict[embedding]
+
+    for get_config_func in get_config_func_list:
+        print("\n===============================")
+        print(embedding)
+        print(get_config_func)
+        kernel_name, measure_name, ck, cm, x_eval = get_config_func()
+        print(ck)
+        print(cm)
+        ke = get_embedding(kernel_name, measure_name, ck, cm)
+        #print(ke)
+
+        x_sample = ke._measure.sample(n)
+        print(x_sample.shape)
+        print(x_eval.shape)
+        y_sample = ke._kernel.evaluate(x_eval, x_sample)
+        print(y_sample.shape)
+        y_mean = y_sample.mean(axis=1)
+        y_std = y_sample.std(axis=1) / np.sqrt(n)
+        print(y_mean.shape)
+        print(y_std.shape)
+
+        for y_mean_i, y_std_i in zip(y_mean, y_std):
+            #print(y_mean_i)
+            #print(y_std_i)
+            print([y_mean_i - ff * y_std_i, y_mean_i + ff * y_std_i])
--- tests/kernel_embedding_dictionary/embeddings/test_embeddings.py
+++ tests/kernel_embedding_dictionary/embeddings/test_embeddings.py
@@ -4,37 +4,57 @@ import numpy as np
 import pytest
 
 from kernel_embedding_dictionary.embeddings import KernelEmbedding
-from kernel_embedding_dictionary import get_embedding
+from kernel_embedding_dictionary._get_embedding import get_embedding
 
 
-@pytest.fixture()
-def config_expquad_lebesgue_uni_1():
+def get_config_expquad_lebesgue_1d_1():
     ck = {"ndim": 1, "lengthscale": [1.0]}
     cm = {"ndim": 1, "bounds": [(0.0, 1.0)]}
-    x = np.array([[1, 0, 1]])
-    mean_intervals = [[0, 1], [], []]
-    return "expquad", "lebesgue", ck, cm, x, mean_intervals
+    x = np.array([[0.1], [0.5], [0.9]])  # 3x1 must lie in domain
+    return "expquad", "lebesgue", ck, cm, x
+
+
+def get_config_expquad_lebesgue_1d_2():
+    ck = {"ndim": 1, "lengthscale": [0.03]}
+    cm = {"ndim": 1, "lengthscale": [(-0.5, 2.5)]}
+    x = np.array([[-0.3], [0.0], [1.5]])  # 3x1 must lie in domain
+    return "expquad", "lebesgue", ck, cm, x
 
 
 @pytest.fixture()
-def config_expquad_lebesgue_uni_2():
-    ck = {"ndim": 1, "lengthscale": [0.3]}
-    cm = {"ndim": 1, "lengthscale": [(-0.5, 2.5)]}
-    mean_interval = [0, 1]
-    return "expquad", "lebesgue", ck, cm, mean_interval
+def config_expquad_lebesgue_1d_1():
+    kn, mn, ck, cm, x = get_config_expquad_lebesgue_1d_1()
+    mean_intervals = [
+        [0.8908900366778463, 0.8928485226983125],
+        [0.9594153133777275, 0.9600861603772526],
+        [0.8905492816317769, 0.8925099674274802],
+    ]
+    return kn, mn, ck, cm, x, mean_intervals
+
+
+@pytest.fixture()
+def config_expquad_lebesgue_1d_2():
+    kn, mn, ck, cm, x = get_config_expquad_lebesgue_1d_2()
+    mean_intervals = [
+        [0.7123375363149562, 0.7153457299975337],
+        [0.8535104942181617, 0.8558150970311784],
+        [0.60567942156233, 0.6088408610742959]
+    ]
+    return kn, mn, ck, cm, x, mean_intervals
 
 
 fixture_list = [
-    "config_expquad_lebesgue_uni_1",
-    "config_expquad_lebesgue_uni_2",
+    "config_expquad_lebesgue_1d_1",
+    "config_expquad_lebesgue_1d_2",
 ]
 
 
 @pytest.mark.parametrize("fixture_name", fixture_list)
 def test_embedding_uni_mean(fixture_name, request):
-    k_name, m_name, ck, cm, mean_intervals, x = request.getfixturevalue(fixture_name)
+    kn, mn, ck, cm, x_eval, mean_intervals = request.getfixturevalue(fixture_name)
 
-    ke = get_embedding(kernel_name=k_name, measure_name=m_name, kernel_config=ck, measure_config=cm)
+    ke = get_embedding(kernel_name=kn, measure_name=mn, kernel_config=ck, measure_config=cm)
 
-    for i, xi in enumerate(x):
-        assert mean_intervals[i, 0] < res[i] < mean_intervals[i, 1]
+    res = ke.mean(x_eval)
+    for i in range(x_eval.shape[0]):
+        assert mean_intervals[i][0] < res[i] < mean_intervals[i][1]
